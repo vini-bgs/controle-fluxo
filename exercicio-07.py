18e49b27e48a4d9008281f010ce0ce84da56791a
vini-bgs <viniciustheros@gmail.com>
2026-06-02 16:58:44 -0300

    atualizacao exercicio 7

--- exercicio-07.py
+++ exercicio-07.py
@@ -1,24 +1,19 @@
 #  Normalizar uma lista de números para que fiquem na escala de 0 a 1.
 
 try:
-    
+
     notas = input("Digite as notas dos alunas separando por vírgula: ")
 
-    lista_notas = []
-
-    for n in notas.split(","):
-        lista_notas.append(int(n))
+    lista_notas = [int(n.strip()) for n in notas.split(",")]
 
     minimo = min(lista_notas)
     maximo = max(lista_notas)
 
-    normalizado = [
-
-        (x - minimo) / (maximo - minimo) for x in lista_notas
-
-    ]
-
+    if minimo == maximo:
+        raise ValueError("Digite pelo menos dois valores diferentes.")
+    
+    normalizado = [(x - minimo) / (maximo - minimo) for x in lista_notas]
     print(normalizado)
-
-except ValueError:
-    print("Erro")
+    
+except ValueError as err:
+    print(f"Atenção! {err}")
