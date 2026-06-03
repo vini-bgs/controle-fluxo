2851bdb23bdd18c5baa37118c09cc8b3b2530826
vini-bgs <viniciustheros@gmail.com>
2026-06-03 13:25:57 -0300

    resolucao do exercicio 11

---
+++ exercicio-11.py
@@ -0,0 +1,16 @@
+# Ler dados de entrada até que uma palavra-chave específica ("sair") seja fornecida.
+
+lista_dados = []
+dado = ""
+
+while True:
+
+    dado = input("Entre com o dado desejado: ")
+
+    if dado.lower().strip() == "sair":
+        print("Encerrando...")
+        break
+
+    lista_dados.append(dado)
+    
+print(lista_dados)
